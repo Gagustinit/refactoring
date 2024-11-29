7db2b01a94b4d05916e5b11aac516f2d380ae90f
Gagustinit <guiag.tx2@gmail.com>
2024-11-29 19:01:21 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -36,4 +36,27 @@ public class HtmlStatement extends Statement {
         "</EM> frequent renter points<P>";
         return result;
      }
+     public String value(Customer aCustomer) {
+        // Criação do cabeçalho para HTML
+    @Override
+    protected String createHeader(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    // Formatação de cada linha de locação para HTML
+    @Override
+    protected String createRentalLine(Rental each) {
+        return each.getMovie().getTitle() + ": " +
+               String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+
+    // Criação do rodapé para HTML
+    @Override
+    protected String createFooter(Customer aCustomer) {
+        return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+               "On this rental you earned <EM>" +
+               String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+               "</EM> frequent renter points<P>";
+    }
+     }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -19,4 +19,24 @@ public class TextStatement extends Statement {
       " frequent renter points";
       return result;
    }
+   // Criação do cabeçalho para texto simples
+   @Override
+   protected String createHeader(Customer aCustomer) {
+       return "Rental Record for " + aCustomer.getName() + "\n";
+   }
+
+   // Formatação de cada linha de locação para texto simples
+   @Override
+   protected String createRentalLine(Rental each) {
+       return "\t" + each.getMovie().getTitle() + "\t" +
+              String.valueOf(each.getCharge()) + "\n";
+   }
+
+   // Criação do rodapé para texto simples
+   @Override
+   protected String createFooter(Customer aCustomer) {
+       return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+              "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+              " frequent renter points";
+   }
 }
