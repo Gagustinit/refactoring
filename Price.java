a96422b0977851c150f082723f2af236e8d5f6df
Gagustinit <guiag.tx2@gmail.com>
2024-11-29 18:35:52 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -26,11 +26,7 @@ public abstract class Price {
         
     }
     public int getFrequentRenterPoints(int daysRented) {
-       
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-            return 2;  
-        }
-        return 1; 
+        return 1;
     }
     
  }
